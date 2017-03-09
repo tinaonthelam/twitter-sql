d6ff79b4b54517aa39f4e0e390d455f4636ef7cf
Nathan Anecone <nanecone.gmail.com>
2017-03-09 17:03:02 -0500

    fussing with tweet display

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ function respondWithAllTweets (req, res, next){
   client.query('SELECT * FROM tweets', function (err, result) {
     if (err) return next(err); // pass errors to Express
     var tweets = result.rows;
-    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+
+      tweets.forEach(function(value){
+     res.render('index', {tweets: value.content});
+    })
   });
+
 }
 
 // here we basically treet the root view and tweets view as identical
@@ -18,13 +22,19 @@ router.get('/tweets', respondWithAllTweets);
 
 // single-user page
 router.get('/users/:username', function(req, res, next){
-  var tweetsForName = tweetBank.find({ name: req.params.username });
-  res.render('index', {
-    title: 'Twitter.js',
-    tweets: tweetsForName,
-    showForm: true,
-    username: req.params.username
-  });
+
+  // client.query()
+
+
+
+
+  // var tweetsForName = tweetBank.find({ name: req.params.username });
+  // res.render('index', {
+  //   title: 'Twitter.js',
+  //   tweets: tweetsForName,
+  //   showForm: true,
+  //   username: req.params.username
+  // });
 });
 
 // single-tweet page
@@ -38,9 +48,27 @@ router.get('/tweets/:id', function(req, res, next){
 
 // create a new tweet
 router.post('/tweets', function(req, res, next){
-  tweetBank.add(req.body.name, req.body.text);
-  res.redirect('/');
-});
+  //inserting a username/tweet into db
+  var userID = 0
+ client.query('SELECT id FROM users WHERE name = $1 ', [req.body.name], function(err, result){
+    if(err) return next(err)
+    console.log(result.rows[0].id, 'ID')
+
+     userID = result.rows[0].id
+
+     client.query('INSERT INTO tweets (user_id, content) VALUES ($1, $2)', [ userID,  req.body.text], function(err, result){
+       if(err) return next(err);
+       res.redirect('/')
+     } )
+   });
+  })
+
+
+
+
+
+  // tweetBank.add(req.body.name, req.body.text);
+  // res.redirect('/');
 
 // // replaced this hard-coded route with general static routing in app.js
 // router.get('/stylesheets/style.css', function(req, res, next){
